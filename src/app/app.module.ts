b194947f706340347a0eab4d7baed453b118dacd
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-08-24 19:59:07 +0300

    Create tower-of-hanoi component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TowerOfHanoiComponent } from './tower-of-hanoi/tower-of-hanoi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TowerOfHanoiComponent
   ],
   imports: [
     BrowserModule
